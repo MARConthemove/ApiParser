59d1c96690749cdef9e6c13ab5b897b9a787f39b
Marc Gaetcke <marc_onthemove@hotmail.com>
2020-10-08 15:09:17 +0200

    changed some code

--- api_git.rb
+++ api_git.rb
@@ -1,8 +1,8 @@
-require "json"
-require "open-uri"
+require 'json'
+require 'open-uri'
 
-puts "What is your GitHub username?"
-puts "> "
+puts 'What is your GitHub username?'
+puts '> '
 
 user_name = gets.chomp.to_s
 
@@ -11,7 +11,6 @@ url = "https://api.github.com/users/#{user_name}"
 serialized_user = open(url).read
 user = JSON.parse(serialized_user)
 
-
 p serialized_user
-puts "-" * 50
+puts '-' * 50
 p user
