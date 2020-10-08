92c9a75bd4d2d22641ce31ffaf33427783e86cc2
Marc Gaetcke <marc_onthemove@hotmail.com>
2020-10-08 14:05:07 +0200

    setup of the api user parser for github

--- api_git.rb
+++ api_git.rb
@@ -7,3 +7,11 @@ puts "> "
 user_name = gets.chomp.to_s
 
 url = "https://api.github.com/users/#{user_name}"
+
+serialized_user = open(url).read
+user = JSON.parse(serialized_user)
+
+
+p serialized_user
+puts "-" * 50
+p user
